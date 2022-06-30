434ff6d68002515c277177904decd39b3ab6f957
HolinWang <holin.wang@outlook.com>
2022-07-01 00:18:23 +0800

    react-router-dom新版本的用法

---
+++ src/AppRouter.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+function Index() {
+  return <h2>JSPang.com</h2>;
+}
+
+function List() {
+  return <h2>List-Page</h2>;
+}
+
+function AppRouter() {
+  return (
+    <Router>
+      <ul>
+        <li> <Link to="/">首页</Link> </li>
+        <li><Link to="/list/">列表</Link> </li>
+      </ul>
+      <Routes>
+        <Route path="/" exact element={<Index />} />
+        <Route path="/list/" element={<List />} />
+      </Routes>
+    </Router>
+  );
+}
+export default AppRouter;
